68e00fd8749f2e5856991e0a34686c688e4a9f30
Alex Wang <ice_b0und@hotmail.com>
2018-11-27 18:16:53 +0800

    [feature] Add benchmark

--- sgd-comm/mpi_win.cpp
+++ sgd-comm/mpi_win.cpp
@@ -10,12 +10,11 @@ using namespace std;
 
 typedef caffe::IOCache<caffe::RetrieveFromSeaweedFS> MyCache;
 
-MyCache *create_cache(vector<string> &file_list) {
-  vector<string> server_list = {"127.0.0.1:8083"};
+MyCache *create_cache(vector<string> &file_list, const vector<string> &server_list, string filename) {
   int64_t worker_count = 8;
 
   string line;
-  ifstream ifs("idlist.train1");
+  ifstream ifs(filename);
   while (!ifs.eof()) {
     getline(ifs, line);
     if (line.size() > 0) {
@@ -32,16 +31,30 @@ MyCache *create_cache(vector<string> &file_list) {
 void use_data(const std::string &name, const int8_t *data, int64_t size) {
   cv::Mat img = cv::imdecode(cv::Mat(vector<char>((char*)data, (char*)data+size)), 0);
   assert(img.data);
-  cout << name << " " << img.rows << "*" << img.cols << endl;
+//  cout << name << " " << img.rows << "*" << img.cols << endl;
 }
 
 int main(int argc, char *argv[]) {
   int rank, nprocs;
-  int64_t size = 65536;
-  char *A = new char[size];
   MPI_Win win;
-  MPI_Datatype column, xpose;
-  int errs = 0;
+
+  const char *filename = argv[1];
+  string masters(argv[2]);
+
+  vector<string> server_list;
+  stringstream server;
+  for (auto c : masters) {
+    if (c == ',') {
+      server_list.push_back(server.str());
+      server.str(""); server.clear();
+    }  else {
+      server << c;
+    }
+  }
+  if (!server.str().empty()) {
+    server_list.push_back(server.str());
+  }
+
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
@@ -56,14 +69,16 @@ int main(int argc, char *argv[]) {
     other_rank = 1;
 
   vector<string> file_list;
-  auto cache = create_cache(file_list);
+  auto cache = create_cache(file_list, server_list, filename);
 
   // epoch 0
+  auto t0 = std::chrono::high_resolution_clock::now();
   for (int64_t i = 0; i < file_list.size(); i++) {
     const int8_t *buf; int64_t buf_size;
     cache->GetOrRetrieve(file_list[i], &buf, &buf_size);
     use_data(file_list[i], buf, buf_size);
   }
+  printf("epoch 0 time: %fs\n", (std::chrono::high_resolution_clock::now()-t0).count()/1e9);
   // TODO: MPI Scatter to let everybody know the file_key -> <offset, len> hash table
 
 //  if (rank == 0) {
@@ -80,7 +95,8 @@ int main(int argc, char *argv[]) {
   MPI_Win_create(cache->buffer(), cache->buffer_size(), sizeof(int8_t), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
   MPI_Win_fence(0, win);
 
-  for (int64_t epoch = 0; epoch < 10; epoch++) {
+  for (int64_t epoch = 1; epoch < 10; epoch++) {
+    auto t0 = std::chrono::high_resolution_clock::now();
     for (int64_t i = 0; i < file_list.size(); i++) {
       auto r = caffe::DefaultDistributionHash(0, i, 2, 1, file_list.size()/2, file_list.size());
       if (rank == r) {
@@ -99,14 +115,15 @@ int main(int argc, char *argv[]) {
         MPI_Win_lock(MPI_LOCK_EXCLUSIVE, other_rank, 0, win);
         MPI_Get(remote_data, buf_size, MPI_BYTE, other_rank, offset, buf_size, MPI_BYTE, win);
         MPI_Win_unlock(other_rank, win);
-        
-        printf("Access remote cache, rank=%d, remote_rank=%d, remote_offset=%ld, remote_size=%ld, valid=%d\n",
-               rank, other_rank, offset, buf_size, memcmp(remote_data, real_buf, buf_size) == 0);
+
+//        printf("Access remote cache, rank=%d, remote_rank=%d, remote_offset=%ld, remote_size=%ld, valid=%d\n",
+//               rank, other_rank, offset, buf_size, memcmp(remote_data, real_buf, buf_size) == 0);
       }
     }
+    printf("epoch %ld time: %fs\n", epoch, (std::chrono::high_resolution_clock::now()-t0).count()/1e9);
   }
 
   MPI_Win_free(&win);
   MPI_Finalize();
-  return errs;
+  return 0;
 }
