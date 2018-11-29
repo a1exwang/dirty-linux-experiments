5d7c0176c9f34f13cebb25763eb65a8c20990a6d
Alex Wang <ice_b0und@hotmail.com>
2018-11-29 12:15:35 +0800

    [feature] Add cache miss

--- sgd-comm/cache.hpp
+++ sgd-comm/cache.hpp
@@ -6,6 +6,7 @@
 #include <functional>
 #include <iostream>
 #include <cstring>
+#include <mpi.h>
 
 namespace caffe {
 
@@ -80,38 +81,46 @@ public:
     return hash_data_.find(key) != hash_data_.end();
   }
 
-  int64_t GetOrRetrieve(const std::string &key, int8_t const* *buf, int64_t *size) {
+  /**
+   * Get from local cache or retrieve from remote file server.
+   * If local cache is full or cache_placement == false,
+   *   local cache is not filled after miss, even if remote_data is correctly fetched.
+   * @param key: file key
+   * @param buf: [OUT] result data
+   * @param size: [OUT] result size
+   * @param cache_placement: if true, place miss result in local cache
+   * @return local cache offset
+   */
+  int64_t GetOrRetrieve(const std::string &key, std::vector<char> &data, bool cache_placement = true) {
     if (Exists(key)) {
       int64_t offset;
-      std::tie(offset, *size) = hash_data_[key];
-      *buf = this->buffer_ + offset;
+      int64_t size;
+      std::tie(offset, size) = hash_data_[key];
+      data.resize(size_t(size));
+      memcpy(data.data(), this->buffer_+offset, size);
       return offset;
     }
 //    std::cerr << "cache miss on '" << key << "'" << std::endl;
     int64_t status_code = 0;
 
-    std::vector<char> data;
     on_miss_.Perform(key, data, status_code);
-    if (current_offset + data.size() > buffer_size_) {
-      // TODO: error reporting
-      std::cerr << "cache buffer overflow" << std::endl;
-      abort();
-    }
-
-    int8_t *current_buf = this->buffer_+current_offset;
-    memcpy(current_buf, data.data(), data.size());
-    hash_data_[key] = std::make_pair(current_offset, data.size());
+//    printf("perform: data size %ld\n", data.size());
 
     int64_t ret = current_offset;
+    int8_t *current_buf = this->buffer_+current_offset;
+
+    if (!cache_placement || current_offset + data.size() > buffer_size_) {
+      return -1;
+    }
+
+    // place in local cache
+    memcpy(current_buf, data.data(), data.size());
+    hash_data_[key] = std::make_pair(current_offset, data.size());
     current_offset += data.size();
-    *buf = current_buf;
-    *size = data.size();
     return ret;
   }
   int8_t *buffer() { return buffer_; }
   int64_t buffer_size() { return buffer_size_; }
-  std::pair<int64_t, int64_t> find(const std::string &key) { return hash_data_[key]; }
-  const std::map<std::string, std::pair<int64_t, int64_t>> &hash_data() const { return hash_data_; }
 private:
   std::vector<std::string> server_list;
   int64_t http_worker_count;
@@ -124,15 +133,113 @@ private:
 };
 
 typedef std::function<int64_t(int64_t seed, int64_t image_id, int64_t processes_per_node, int64_t nodes, int64_t total)> DistributionHash;
-static inline int64_t DefaultDistributionHash(int64_t _seed, int64_t image_id, int64_t processes_per_node, int64_t nodes, int64_t images_per_process, int64_t total_images) {
-  int64_t total_cached_imgaes = processes_per_node * nodes * images_per_process;
-  int64_t total_process = processes_per_node * nodes;
-//  if (total_cached_imgaes >= total_images) {
-//    return int64_t(image_id / (double(total_images) / total_process));
-//  } else {
-//    return int64_t(image_id / (double(total_cached_imgaes) / total_process));
-//  }
-  return image_id % (processes_per_node * nodes);
+static inline int64_t DefaultDistributionHash(int64_t _seed, int64_t image_id, int64_t processes_per_node, int64_t nodes, int64_t cached_images_per_process, int64_t total_images) {
+  int64_t total_processes = processes_per_node * nodes;
+  int64_t total_cached_images = total_processes * cached_images_per_process;
+
+  if (image_id >= total_cached_images) {
+    return -1;
+  } else {
+    return image_id % total_processes;
+  }
 }
 
-}
+typedef caffe::IOCache<caffe::RetrieveFromSeaweedFS> MyCache;
+
+enum CacheHitType {
+  Local,
+  Cluster,
+  Miss
+};
+class DistributedCache {
+public:
+  DistributedCache(const std::vector<std::string> &server_list, int64_t http_worker_count, int64_t buffer_size,
+      int64_t image_count, int rank, std::function <int (int64_t)> f)
+    :rank(rank), f(std::move(f)), cache(server_list, http_worker_count, buffer_size),
+     image_count(image_count), global_index_((int64_t*)calloc(image_count*2, sizeof(int64_t))) {
+  }
+  ~DistributedCache() {
+    free(this->global_index_);
+  }
+  std::tuple<const int8_t*, int64_t> Get(
+      const std::vector<std::string> &file_list,
+      int64_t image_id,
+      bool fill_global_index,
+      bool use_cluster_cache,
+      CacheHitType *hitType = nullptr) {
+    int hash_rank = f(image_id);
+    int8_t *image_data = nullptr;
+    if (hash_rank == rank) {
+      // local cache hit
+      if (hitType != nullptr) {
+        *hitType = CacheHitType::Local;
+      }
+      std::vector<char> data;
+      int64_t offset = cache.GetOrRetrieve(file_list[image_id], data);
+//      printf("cache.GetOrRetrieve: (offset, size) = (%ld, %ld)\n", offset, data.size());
+
+      if (fill_global_index) {
+        this->set(image_id, std::make_tuple(offset, data.size()));
+      }
+
+      image_data = new int8_t[data.size()];
+      memcpy(image_data, data.data(), (size_t) data.size());
+      return std::make_tuple(image_data, data.size());
+    } else if (!use_cluster_cache || hash_rank < 0) {
+      if (hitType != nullptr) {
+        *hitType = CacheHitType::Miss;
+      }
+      return this->cacheMiss(file_list[image_id]);
+    } else {
+      // cluster cache hit and use_cluster_cache
+      if (hitType != nullptr) {
+        *hitType = CacheHitType::Cluster;
+      }
+      return clusterCacheHit(image_id, hash_rank);
+    }
+  }
+
+  void Sync() {
+    MPI_Allreduce(MPI_IN_PLACE, this->global_index_, int(image_count*2), MPI_INT64_T, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Win_create(cache.buffer(), cache.buffer_size(), sizeof(int8_t), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    MPI_Win_fence(0, win);
+  }
+
+  std::tuple<const int8_t*, int64_t> cacheMiss(const std::string &file_name) {
+    std::vector<char> data;
+    cache.GetOrRetrieve(file_name, data, false);
+    auto image_data = new int8_t[data.size()];
+    memcpy(image_data, data.data(), data.size());
+    return std::make_tuple(image_data, data.size());
+  }
+
+  std::tuple<const int8_t*, int64_t> clusterCacheHit(int64_t image_id, int target_rank) {
+    int64_t offset; int64_t buf_size;
+    std::tie(offset, buf_size) = this->at(image_id);
+    auto image_data = new int8_t[buf_size];
+
+    MPI_Win_lock(MPI_LOCK_EXCLUSIVE, target_rank, 0, win);
+    MPI_Get(image_data, (int) buf_size, MPI_BYTE, target_rank, offset, (int) buf_size, MPI_BYTE, win);
+    MPI_Win_unlock(target_rank, win);
+    return std::make_tuple(image_data, buf_size);
+  }
+
+private:
+
+  inline std::tuple<int64_t, int64_t> at(int64_t image_id) const {
+    return std::make_tuple(this->global_index_[image_id*2 + 0], this->global_index_[image_id*2 + 1]);
+  }
+  inline void set(int64_t image_id, std::tuple<int64_t, int64_t> offset_size){
+    std::tie(this->global_index_[image_id*2 + 0], this->global_index_[image_id*2 + 1]) = offset_size;
+  }
+
+  int64_t image_count;
+  int64_t *global_index_;
+private:
+  MPI_Win win;
+  MyCache cache;
+  int rank;
+  std::function <int (int64_t)> f;
+};
+
+};
--- sgd-comm/mpi_win.cpp
+++ sgd-comm/mpi_win.cpp
@@ -11,25 +11,19 @@
 
 using namespace std;
 
-typedef caffe::IOCache<caffe::RetrieveFromSeaweedFS> MyCache;
 
-MyCache *create_cache(vector<string> &file_list, const vector<string> &server_list, string filename) {
-  int64_t worker_count = 8;
-
+void parse_file_list(vector<string> &file_list, const string &filename) {
   string line;
   ifstream ifs(filename);
   while (!ifs.eof()) {
     getline(ifs, line);
-    if (line.size() > 0) {
+    if (!line.empty()) {
       auto off = line.find(' ');
       auto file_path = line.substr(0, off);
       file_list.push_back(file_path);
       line.substr(off+1, line.size() - off);
     }
   }
-
-  int64_t buffer_size = file_list.size() * 200 * 1024;
-  return new MyCache(server_list, worker_count, buffer_size);
 }
 
 void use_data(const std::string &name, const int8_t *data, int64_t size) {
@@ -37,44 +31,57 @@ void use_data(const std::string &name, const int8_t *data, int64_t size) {
   assert(img.data);
 }
 
-class GlobalCache {
-public:
-  GlobalCache(int64_t image_count)
-    :image_count(image_count),
-      data_((int64_t*)calloc(image_count*2, sizeof(int64_t))) {
-  }
-  ~GlobalCache() {
-    free(data_);
+void epoch_loop(const std::vector<string> &image_list, const function<int(int64_t)> &f, int rank,
+                int64_t print_size, int64_t epoch,
+                const function<int64_t (int64_t image_id)> &process_image) {
+  static double epoch0_iops = 1;
+  int64_t total_size = 0;
+  auto epoch_start = std::chrono::high_resolution_clock::now();
+  auto t0 = std::chrono::high_resolution_clock::now();
+  for (int64_t image_id = 0; image_id < image_list.size(); image_id++) {
+//    printf("image_id = %ld\n", image_id);
+
+    auto size = process_image(image_id);
+
+    total_size += size;
+
+    if (image_id % print_size == 0) {
+      auto t1 = std::chrono::high_resolution_clock::now();
+      auto deltat = (t1-t0).count()/1e9;
+      double iops = 1/deltat * print_size;
+      double tp = total_size / deltat / 1024/1024;
+      MPI_Allreduce(MPI_IN_PLACE, &iops, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+      MPI_Allreduce(MPI_IN_PLACE, &tp, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+      if (rank == 0) {
+        printf("epoch=%ld time=%fs IOPS=%f tp=%.2fMiB/s i=%ld\n", epoch, deltat, iops, tp, image_id);
+      }
+      t0 = std::chrono::high_resolution_clock::now();
+      total_size = 0;
+    }
   }
 
-  inline tuple<int64_t, int64_t> at(int64_t image_id) const {
-    return make_tuple(this->data_[image_id*2 + 0], this->data_[image_id*2 + 1]);
+  double epoch_time = (std::chrono::high_resolution_clock::now()-epoch_start).count()/1e9;
+  double epoch_iops = image_list.size() / epoch_time;
+  MPI_Allreduce(MPI_IN_PLACE, &epoch_iops, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  if (epoch == 0) {
+    epoch0_iops = epoch_iops;
   }
-  inline void set(int64_t image_id, int64_t offset, int64_t buf_size){
-    this->data_[image_id*2 + 0] = offset;
-    this->data_[image_id*2 + 1] = buf_size;
+  if (rank == 0) {
+    printf("epoch %ld done, epoch_time=%.2f, epoch_iops=%0.2f, acceleration=%.2fx\n", epoch, epoch_time, epoch_iops, epoch_iops/epoch0_iops);
   }
-
-  void sync() {
-    MPI_Allreduce(MPI_IN_PLACE, this->data_, int(image_count*2), MPI_INT64_T, MPI_SUM, MPI_COMM_WORLD);
-  }
-private:
-  int64_t image_count;
-  int64_t *data_;
-};
+}
 
 
 int main(int argc, char *argv[]) {
   int rank, nprocs;
-  MPI_Win win;
-
   const char *filename = argv[1];
   string masters(argv[2]);
-  int nodes = atoi(argv[3]);
-  int images_per_proc = atoi(argv[4]);
-  int do_decode = atoi(argv[5]);
-  int64_t print_size = atol(argv[6]);
+  int64_t nodes = strtol(argv[3], nullptr, 10);
+  int64_t images_per_proc = strtol(argv[4], nullptr, 10);
+  int64_t do_decode = strtol(argv[5], nullptr, 10);
+  int64_t print_size = strtol(argv[6], nullptr, 10);
 
+  // parse server list
   vector<string> server_list;
   stringstream server;
   for (auto c : masters) {
@@ -89,128 +96,59 @@ int main(int argc, char *argv[]) {
     server_list.push_back(server.str());
   }
 
+  // parse file list
+  vector<string> file_list;
+  parse_file_list(file_list, filename);
+
+  // init mpi
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  // assume 200K per image
 
-  vector<string> file_list;
-  auto cache = create_cache(file_list, server_list, filename);
+  // init cache
+  int64_t buffer_size = file_list.size() * 200 * 1024;
   int64_t image_count = file_list.size();
   auto f = [=](int64_t image_id) -> int {
     return (int)caffe::DefaultDistributionHash(0, image_id, nprocs/nodes, nodes, images_per_proc, image_count);
   };
-
-  // image_id -> (offset, len)
-  GlobalCache global_index(image_count);
+  caffe::DistributedCache distributedCache(server_list, 8, buffer_size, image_count, rank, f);
 
   // epoch 0
-  int64_t buf_size_total = 0;
-  auto epoch0_start = std::chrono::high_resolution_clock::now();
-  auto t0 = std::chrono::high_resolution_clock::now();
-  int64_t i_local = 0;
-  for (int64_t image_id = 0; image_id < image_count; image_id++) {
-    const int8_t *buf;
-    int64_t buf_size;
-    // only file buffer for local
-    int r = f(image_id);
-    if (r == rank) {
-      i_local++;
-      int64_t offset = cache->GetOrRetrieve(file_list[image_id], &buf, &buf_size);
-      if (do_decode)
-        use_data(file_list[image_id], buf, buf_size);
+  epoch_loop(file_list, f, rank, print_size, 0, [&](int64_t image_id) -> int64_t {
+    const int8_t *buf; int64_t buf_size;
+    tie(buf, buf_size) = distributedCache.Get(file_list, image_id, true, false);
+    if (do_decode)
+      use_data(file_list[image_id], buf, buf_size);
+    return buf_size;
+  });
 
-      global_index.set(image_id, offset, buf_size);
-
-      buf_size_total += buf_size;
-      if (i_local % print_size == 0) {
-        auto t1 = std::chrono::high_resolution_clock::now();
-        auto deltat = (t1-t0).count()/1e9;
-        double iops = 1/deltat * print_size;
-        double tp = buf_size_total / deltat / 1024/1024;
-        MPI_Allreduce(MPI_IN_PLACE, &iops, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-        MPI_Allreduce(MPI_IN_PLACE, &tp, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-        if (rank == 0) {
-          printf("epoch=0 time=%fs IOPS=%f tp=%.2fMiB/s i=%ld\n", deltat, iops, tp, i_local);
-        }
-        t0 = std::chrono::high_resolution_clock::now();
-        buf_size_total = 0;
-      }
-    }
-  }
-  double epoch0_time = (std::chrono::high_resolution_clock::now()-epoch0_start).count()/1e9;
-  double epoch0_iops = i_local / epoch0_time;
-  MPI_Allreduce(MPI_IN_PLACE, &epoch0_iops, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-  if (rank == 0) {
-    printf("Epoch 0 done, syncing map\n");
-  }
-  global_index.sync();
-  if (rank == 0) {
-    printf("Epoch 0 done, starting other epochs, rank=%d\n", rank);
-  }
+  // sync global index
   MPI_Barrier(MPI_COMM_WORLD);
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-  MPI_Win_create(cache->buffer(), cache->buffer_size(), sizeof(int8_t), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
-  MPI_Win_fence(0, win);
+  distributedCache.Sync();
+  MPI_Barrier(MPI_COMM_WORLD);
 
   int64_t total_epochs = 10;
-  for (int64_t epoch = 1; epoch <= total_epochs; epoch++) {
-    buf_size_total = 0;
-    auto epoch_start = std::chrono::high_resolution_clock::now();
-    auto t0 = std::chrono::high_resolution_clock::now();
-    for (int64_t image_id = 0; image_id < file_list.size(); image_id++) {
-      int r = f(image_id);
-      if (r == rank) {
-        // local cache hit
-        const int8_t *buf; int64_t buf_size;
-        cache->GetOrRetrieve(file_list[image_id], &buf, &buf_size);
-        if (do_decode)
-          use_data(file_list[image_id], buf, buf_size);
-        buf_size_total += buf_size;
-      } else if (r < 0) {
-        // no cache hit
+  caffe::CacheHitType hit_type;
+  int64_t hits[3] = {0};
+  for (int64_t epoch = 1; epoch < total_epochs; epoch++) {
+    epoch_loop(file_list, f, rank, print_size, epoch, [&](int64_t image_id) -> int64_t {
+      const int8_t *image_data = nullptr;
+      int64_t buf_size = 0;
 
-      } else {
-        // cluster cache hit
-        int64 offset, buf_size;
-        tie(offset, buf_size) = global_index.at(image_id);
-        auto remote_data = new int8_t[buf_size];
+      // TODO: improve memory management
+      tie(image_data, buf_size) = distributedCache.Get(file_list, image_id, false, true, &hit_type);
+      hits[hit_type]++;
 
-        MPI_Win_lock(MPI_LOCK_EXCLUSIVE, r, 0, win);
-        MPI_Get(remote_data, buf_size, MPI_BYTE, r, offset, buf_size, MPI_BYTE, win);
-        MPI_Win_unlock(r, win);
+      if (do_decode)
+        use_data(file_list[image_id], image_data, buf_size);
+      delete []image_data;
+      return buf_size;
+    });
 
-        if (do_decode)
-          use_data(file_list[image_id], remote_data, buf_size);
-
-        buf_size_total += buf_size;
-        delete []remote_data;
-      }
-      if (image_id % print_size == 0) {
-        auto t1 = std::chrono::high_resolution_clock::now();
-        auto deltat = (t1-t0).count()/1e9;
-        double iops = 1/deltat * print_size;
-        double tp = buf_size_total / deltat / 1024/1024;
-        MPI_Allreduce(MPI_IN_PLACE, &iops, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-        MPI_Allreduce(MPI_IN_PLACE, &tp, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-        if (rank == 0) {
-          printf("epoch=%ld time=%fs IOPS=%.2f tp=%.2fMiB/s i=%ld\n", epoch, deltat, iops, tp, image_id);
-        }
-        t0 = std::chrono::high_resolution_clock::now();
-        buf_size_total = 0;
-      }
-    }
-    // Epoch barrier
-    MPI_Barrier(MPI_COMM_WORLD);
-    double epoch_time = (std::chrono::high_resolution_clock::now()-epoch_start).count()/1e9;
-    double epoch_iops = image_count / epoch_time;
-    MPI_Allreduce(MPI_IN_PLACE, &epoch_iops, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-    if (rank == 0)
-      printf("epoch %ld time: %fs, acceleration: %.2f times.\n", epoch, epoch_time, epoch_iops/epoch0_iops*nprocs);
+    double total = hits[0] + hits[1] + hits[2];
+    printf("rank %d, cache hit rate: local %.2f%%, cluster %.2f%%, miss %.2f%%\n", rank, hits[0]/total*100, hits[1]/total*100, hits[2]/total*100);
   }
 
-  MPI_Win_free(&win);
   MPI_Finalize();
   return 0;
 }
